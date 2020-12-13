b6bbafbe132994ba6b8effe2a843acc60b1eee80
Valentina Carrington <valcondur@gmail.com>
2020-12-12 22:11:48 -0700

    Initial Commit

---
+++ friends-social/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  friends-social
+//
+//  Created by Valentina Carrington on 12/12/20.
+//  Copyright © 2020 Valentina Carrington. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
